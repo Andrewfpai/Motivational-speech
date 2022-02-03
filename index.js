1ed786d09b534443d495dc62a1c5af2aedb376bf
Andrewfpai <nicolasandrew.28@gmail.com>
2022-02-03 16:35:36 +0700

    testing2

--- index.js
+++ index.js
@@ -14,17 +14,81 @@ function myDate(){
     document.getElementById("clock").textContent = display
 
     setTimeout(myDate, 1000);
-
-    if (h < 7){
-        document.getElementById("bg").textContent = "morning";
-        
-    } else if (h>6 && h<12){
-        document.getElementById("bg").textContent = "afternoon"
-    } else if (h>11 && h<18){
-        document.getElementById("bg").textContent = "evening"
-    } else if (h>17 && h<25){
-        document.getElementById("bg").textContent = "night"
-        document.body.style.backgroundImage = "url('images/Afternoon.png')";
-    }
+    
 }
+
+
+function nothing(){
+    h = new Date().getHours()
+   
+
+    let caption = document.getElementById("bg")
+    let bg = document.body.style
+    let time
+
+    if (h < 6){
+        caption.textContent = "Good Morning";
+        bg.backgroundImage = "url('images/Night_three.png')";
+        time = 0
+
+        
+    } else if (h>5 && h<11){
+        caption.textContent = "Good Morning"
+        bg.backgroundImage = "url('images/Morning.png')";
+        time = 0
+        
+    } else if (h>10 && h<15){
+        caption.textContent = "Good Afternoon"
+        bg.backgroundImage = "url('images/Afternoon.png')";
+        time = 1
+    
+    } else if (h>14 && h<18){
+        caption.textContent = "Good Evening"
+        bg.backgroundImage = "url('images/Evening.png')";
+        time = 2
+    
+    } else if (h>17 && h<25){
+        caption.textContent = "Good Night"
+        bg.backgroundImage = "url('images/Night_one.png')";
+        time = 3
+    }
+
+    let text = caption.textContent
+    return time
+}
+
+let x;
+x=2
+
+
+function bgTranslate(){
+    const indonesia = ["Selamat Pagi", "Selamat Siang", "Selamat Sore", "Selamat Malam"]
+    const english = ["Good Morning", "Good Afternoon", "Good Evening", "Good Night"]
+    const chinese = ["早上好", "中午好", "下午好", "晚上好"  ]
+
+
+    let timeLocal  = nothing();
+    let caption = document.getElementById("bg")
+    
+
+
+    if (x===1){ // indonesia to english
+        caption.textContent = english[timeLocal]
+        x+= 1
+    } else if (x===2){ // english to chinese
+        caption.textContent = chinese[timeLocal]
+        x+= 1
+    } else if (x===3){ // chinese to indonesia
+        caption.textContent = indonesia[timeLocal]
+        x+= 1
+    } else if (x>3) {
+        x = 2
+    }
+
+
+
+
+}
+
+
 
